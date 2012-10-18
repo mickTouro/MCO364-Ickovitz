8f8e59c9f117d1c79b8ecc0d33f3d60a2ec75310
mickTouro <mickovitz@yahoo.com>
2012-10-17 20:17:10 -0400

    Apatche IO Utils

--- src/ickovitz/dictionary/DictionaryComparison.java
+++ src/ickovitz/dictionary/DictionaryComparison.java
@@ -3,45 +3,64 @@ package ickovitz.dictionary;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.apache.commons.io.IOUtils;
 
 public class DictionaryComparison {
 
 	private ArrayList<String> words = new ArrayList<String>();
+	private String[] wordArray = new String[113809];
 
 	public DictionaryComparison() {
-		this.fillWordArray("G:\\workspace1\\MCO364-Ickovitz\\word_list_moby_crossword.flat.txt");
+		this.fillWordArrays("word_list_moby_crossword.flat.txt");
 
 	}
-	
-	public DictionaryComparison(String fileName){
-		this.fillWordArray(fileName);
+
+	public DictionaryComparison(String fileName) {
+		this.fillWordArrays(fileName);
 	}
 
-	public void fillWordArray(String fileName) {
+	public void fillWordArrays(String fileName) {
 
 		try {
+			
+			
 			BufferedReader reader = new BufferedReader(new FileReader(fileName));
-
-			String word = reader.readLine();
-			while (word != null) {
-				words.add(word);
-				word = reader.readLine();
-			}
+			words = IOUtils.readLines(reader);
+			
+			reader.close();
 
 		} catch (Exception e) {
 
 			e.printStackTrace();
 		}
+
+		int index = 0;
+		for (String s : words) {
+			
+			wordArray[index] = s;
+			index++;
+		}
+
 	}
 
-	public boolean isWord(String word) {
-		boolean bool = false;
-		for (int index = 0; index < words.size(); index++) {
-			if (words.get(index).equals(word)) {
-				bool = true;
-				break;
+	public boolean containsSequentialSearch(String word) {
+		for (String s : words) {
+			if (word.equals(s)) {
+				return true;
 			}
 		}
-		return bool;
+		return false;
+	}
+
+	public boolean containsBinarySearch(String word) {
+		int result = Arrays.binarySearch(wordArray, word);
+
+		if (result != -1)
+			return true;
+		else
+			return false;
+
 	}
 }
